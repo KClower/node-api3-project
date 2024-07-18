516b898a5d11ac8474e2c48dbfa08a7c933dd40f
KClower <kclower3@gmail.com>
2024-07-17 19:57:39 -0500

    got all the routes to work, still need middleware added to routes

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,27 @@
+const userModel = require('../users/users-model.js');
+
+
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+  );
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const id = req.params.id;
+  User.getById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(req.user);
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+  next();
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +33,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+};
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+});
